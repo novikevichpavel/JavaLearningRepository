4e581d61cd69a7a59ad408f56779246bb432b5f4
novikevichpavel <novikevichp@gmail.com>
2026-02-20 22:30:49 +0300

    Add fist part of homework

---
+++ scr/Homeworks/BankAccount.java
@@ -0,0 +1,46 @@
+package Homeworks;
+
+public class BankAccount {
+
+    BankAccount(String customerName, int customerId, double customerBalance){
+        name = customerName;
+        id = customerId;
+        balance = customerBalance;
+    }
+
+    String name;
+    int id;
+    double balance;
+
+    double riseSum (double upValue){
+        balance = balance + upValue;
+        return balance;
+    }
+
+    double downSum (double downSum){
+        balance = balance - downSum;
+        return balance;
+    }
+
+    void showInfo(){
+
+        System.out.println("User " + name + " has id: " + id + " and " + balance + " on his bank account");
+
+    }
+
+}
+
+class BankAccountTets {
+
+    public static void main(String[] args) {
+
+        BankAccount myAccount = new BankAccount("Pavel", 27, 470);
+        myAccount.riseSum(230);
+        myAccount.showInfo();
+
+    }
+
+}
+
+
+
